cfb9772d2308ef066ac67321be0b2fa819a5fb72
mitnikkozo <migirulz44@gmail.com>
2020-07-05 03:13:57 +1200

    upload many files to db

--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const sharp = require("sharp");
 const multer = require('multer');
+const bodyParser = require('body-parser');
 
 /////////////
 //Get models
 /////////////
 
 const cardModel = require('../models/cards').Card;
-
-
 
 ///////////////
 //Config Multer
@@ -26,6 +25,12 @@ const upload = multer({
 
 const router = new express.Router();
 
+///////////////
+//config route
+//////////////
+
+router.use(bodyParser.urlencoded({extended: false}));
+router.use(bodyParser.json());
 
 //home
 router.get("/", (req, res) => {
@@ -40,14 +45,28 @@ router.get("/upload", (req, res) => {
 
 
 //convert to png and save as a buffer to the db
-router.post("/upload", upload.single('upload'), async (req, res) => {
+router.post("/upload", upload.fields([{name: "upload", maxCount: 1}, {name: "imgs", maxCount: 20}]), async (req, res) => {
+
+    try{
+        const buf = await sharp(req.files.upload[0].buffer).resize({width: 250, height: 250}).png().toBuffer();
+        let imgs = new Array;
+
+        for(let i = 0; i < req.files.imgs.length; i++){
+            let img = await sharp(req.files.imgs[i].buffer).resize({width: 250, height: 250}).png().toBuffer();
+            imgs.push(img);
+        }
+        
+        let card = new cardModel({name: req.body.name, showImage: buf, description: req.body.desc, images: imgs});
+        
+        card.save();
+        res.redirect("/");
+    }catch(e){
+        console.log(e);
+        res.redirect("back");
+    }
     
-    const buf = await sharp(req.file.buffer).resize({width: 250, height: 250}).png().toBuffer();
-    let card = new cardModel({name: req.body.name, showImage: buf, description: req.body.desc});
-    
-    card.save();
-    res.redirect("/");
 }, (error, req, res, next) => {
+    console.log(error);
     res.status(400).send({error : error.message});
 });
 
